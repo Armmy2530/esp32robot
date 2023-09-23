ac20e811ae6ce1ec1028c157884988e7bef4101e
Armmy2530 <sutigran2557@gmail.com>
2023-09-24 01:30:46 +0700

    add .gitignore

--- joystick/main/.build/sketch/.build/build_opt.h
+++
@@ -1,1 +0,0 @@
-#line 1 "D:\\Documents\\Project\\esp32robot\\joystick\\main\\.build\\build_opt.h"
--- joystick/main/.build/sketch/.build/sketch/.build/build_opt.h
+++
@@ -1,2 +0,0 @@
-#line 1 "D:\\Documents\\Project\\esp32robot\\joystick\\main\\.build\\sketch\\.build\\build_opt.h"
-#line 1 "D:\\Documents\\Project\\esp32robot\\joystick\\main\\.build\\build_opt.h"
